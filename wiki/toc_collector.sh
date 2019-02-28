1874224083e1433386cc7c33a8a647de6de38bab
Ales Puchilo <ales.puchilo@gmail.com>
2019-02-28 12:07:35 +0700

    reorg docs again

--- wiki/toc_collector.sh
+++ wiki/toc_collector.sh
@@ -4,7 +4,3 @@ cat config.yml >> mkdocs.yml
 cat docs/cyb/cyb.yml >> mkdocs.yml
 cat docs/cyberd/cyberd.yml >> mkdocs.yml
 cat docs/сhaingear/chaingear.yml >> mkdocs.yml
-echo "  - help:" >> mkdocs.yml
-cat docs/help/cyb/cyb.yml >> mkdocs.yml
-cat docs/help/cyberd/cyberd.yml >> mkdocs.yml
-cat docs/help/chaingear/chaingear.yml >> mkdocs.yml
