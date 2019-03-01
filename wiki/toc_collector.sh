e15e0d3171f3b6dddbb0e5dd3ebd28d474c562f3
Ales Puchilo <ales.puchilo@gmail.com>
2019-03-01 15:18:08 +0700

    replace NAV

--- wiki/toc_collector.sh
+++ wiki/toc_collector.sh
@@ -1,10 +1,10 @@
 rm -rf ./mkdocs.yml
 touch mkdocs.yml
 cat config.yml >> mkdocs.yml
+echo "nav:" >> mkdocs.yml
 cat docs/blog/blog.yml >> mkdocs.yml
 cat docs/cyb/cyb.yml >> mkdocs.yml
 cat docs/cyberd/cyberd.yml >> mkdocs.yml
 cat docs/сhaingear/chaingear.yml >> mkdocs.yml
-echo "nav:" >> mkdocs.yml
 echo "  - cyber•Congress: index.md" >> mkdocs.yml
 echo "  - Contributing: contribute.md" >> mkdocs.yml
